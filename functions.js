8562dc529d0953caa6bf9a13d46dbe6592d71bb1
juanjo <juanjo.mdt@gmail.com>
2023-04-12 08:00:18 -0300

    actualizacion scss

---
+++ functions.js
@@ -0,0 +1,59 @@
+ $(document).ready(function () {
+  let estadoOculto = false;
+  let pregunta = $("#hidden-text");
+  let btn=$("#hidden-btn")
+ 
+  btn.click(function () {
+    if (estadoOculto) {
+      pregunta.hide();
+      estadoOculto=false;
+    } else{
+      pregunta.show();
+        estadoOculto=true;
+      }
+    }
+  )});
+
+
+  let estadoOculto2 = false;
+  let btn2=$("#hidden-btn2")
+  let pregunta2 = $("#hidden-text2");
+ btn2.click(function () {
+  if (estadoOculto2) {
+    pregunta2.hide();
+    estadoOculto2=false;
+  } else {
+    pregunta2.show();
+    estadoOculto2=true;
+    
+  }
+ })
+  
+
+   let estadoOculto3 = false;
+   let pregunta3 = $("#hidden-text3");
+   let btn3=$("#hidden-btn3")
+  btn3.click(function () {
+    if (estadoOculto3) {
+      pregunta3.hide();
+      estadoOculto3=false;
+    } else {
+      pregunta3.show();
+      estadoOculto3=true;
+      
+    }
+  })
+
+  let estadoOculto4 = false;
+  let pregunta4 = $("#hidden-text4");
+  let btn4=$("#hidden-btn4")
+
+  btn4.click(function () {
+    if (estadoOculto4) {
+      pregunta4.hide();
+      estadoOculto4=false;
+    } else {
+      pregunta4.show();
+      estadoOculto4=true;
+    }
+  })
---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = document.querySelectorAll('.question');
+const answers = document.querySelectorAll('.answer');
+
+questions.forEach((question, index) => {
+  question.addEventListener('click', () => {
+    answers[index].style.display = 'block';
+  });
+
+  answers[index].addEventListener('click', () => {
+    answers[index].style.display = 'none';
+  });
+});
