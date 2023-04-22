e8ebf23466f5fb54dc4bbfa7e522a0fc7d294d18
ivan2374 <ivanmartines@hotmail.com>
2023-04-22 00:27:04 -0300

    Se mejora el codigo y se agregan estilos nuevos a la seccion de reseñas

--- reseñas.js
+++ reseñas.js
@@ -1,199 +1,83 @@
+//Funcion que genera un numero aleatorio que se usa como indice para obtener los datos de la api rest
+let numeroRandom = (min, max) =>
+    Math.floor(Math.random() * (max - min)) + min;
 
-let random = Math.floor(Math.random() * 30)
-$.ajax({
-    method: 'GET',
-    url: `https://powergym-adm-default-rtdb.firebaseio.com/${random}.json`,
-    headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-    contentType: 'application/json',
-    success: function (results) {
-
-        let quote = document.querySelector(".frase");
-        let autor = document.querySelector(".autor")
-        let foto = document.querySelector(".img")
-        quote.innerHTML = results.reseñas
-        autor.innerHTML = results.autor
-        foto.src = results.foto
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-
-});
-
-
-let randomUno = Math.floor(Math.random() * 30)
-$.ajax({
-    method: 'GET',
-    url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomUno}.json`,
-    headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-    contentType: 'application/json',
-    success: function (result) {
-
-        let quote = document.querySelector(".frase_primer");
-        let autor = document.querySelector(".autor_primer")
-        let foto = document.querySelector(".img_primer")
-        quote.innerHTML = result.reseñas
-        autor.innerHTML = result.autor
-        foto.src = result.foto
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-
-});
-
-let randomDos = Math.floor(Math.random() * 30)
-$.ajax({
-    method: 'GET',
-    url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomDos}.json`,
-    headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-    contentType: 'application/json',
-    success: function (result) {
-
-        let quote = document.querySelector(".frase_seg");
-        let autor = document.querySelector(".autor_seg")
-        let foto = document.querySelector(".img_seg")
-        quote.innerHTML = result.reseñas
-        autor.innerHTML = result.autor
-        foto.src = result.foto
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-
-});
-let randomTres = Math.floor(Math.random() * 30)
-$.ajax({
-    method: 'GET',
-    url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomTres}.json`,
-    headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-    contentType: 'application/json',
-    success: function (result) {
-
-        let quote = document.querySelector(".frase_tercer");
-        let autor = document.querySelector(".autor_tercer")
-        let foto = document.querySelector(".img_tercer")
-        quote.innerHTML = result.reseñas
-        autor.innerHTML = result.autor
-        foto.src = result.foto
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-
-});
-
-let slider = document.querySelector(".slider-contenedor");
-let sliderIndividual = document.querySelectorAll(".slider-test");
-let contador = 1
-let tamanoWidth = sliderIndividual[0].clientWidth;
-let intervalo = 5000;
-
-window.addEventListener("resize", function () {
-    tamanoWidth = sliderIndividual[0].clientWidth;
-})
-
-setInterval(function tiempo() {
-    slices();
-}, intervalo);
-
-
-function slices() {
-    slider.style.transform = 'translate(' + (- tamanoWidth * contador) + 'px)';
-    slider.style.transition = 'transform 1s';
-    contador++;
-
-    if (contador == sliderIndividual.length) {
-        contador = 0;
-        setTimeout(function () {
-            slider.style.transform = 'translate(0px)';
-            slider.style.transition = 'transform 0s';
-            let random = Math.floor(Math.random() * 30)
-            $.ajax({
-                method: 'GET',
-                url: `https://powergym-adm-default-rtdb.firebaseio.com/${random}.json`,
-                headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-                contentType: 'application/json',
-                success: function (results) {
-
-                    let quote = document.querySelector(".frase");
-                    let autor = document.querySelector(".autor")
-                    let foto = document.querySelector(".img")
-                    quote.innerHTML = results.reseñas
-                    autor.innerHTML = results.autor
-                    foto.src = results.foto
-                },
-                error: function ajaxError(jqXHR) {
-                    console.error('Error: ', jqXHR.responseText);
-                }
-
-            });
-
-
-            let randomUno = Math.floor(Math.random() * 30)
-            $.ajax({
-                method: 'GET',
-                url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomUno}.json`,
-                headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-                contentType: 'application/json',
-                success: function (result) {
-
-                    let quote = document.querySelector(".frase_primer");
-                    let autor = document.querySelector(".autor_primer")
-                    let foto = document.querySelector(".img_primer")
-                    quote.innerHTML = result.reseñas
-                    autor.innerHTML = result.autor
-                    foto.src = result.foto
-                },
-                error: function ajaxError(jqXHR) {
-                    console.error('Error: ', jqXHR.responseText);
-                }
-
-            });
-
-            let randomDos = Math.floor(Math.random() * 30)
-            $.ajax({
-                method: 'GET',
-                url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomDos}.json`,
-                headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-                contentType: 'application/json',
-                success: function (result) {
-
-                    let quote = document.querySelector(".frase_seg");
-                    let autor = document.querySelector(".autor_seg")
-                    let foto = document.querySelector(".img_seg")
-                    quote.innerHTML = result.reseñas
-                    autor.innerHTML = result.autor
-                    foto.src = result.foto
-                },
-                error: function ajaxError(jqXHR) {
-                    console.error('Error: ', jqXHR.responseText);
-                }
-
-            });
-            let randomTres = Math.floor(Math.random() * 30)
-            $.ajax({
-                method: 'GET',
-                url: `https://powergym-adm-default-rtdb.firebaseio.com/${randomTres}.json`,
-                headers: { 'X-Api-Key': 'fGHsEYf2bTch4u3yMZeREA==IFDxJlG0BTjYf9TE' },
-                contentType: 'application/json',
-                success: function (result) {
-
-                    let quote = document.querySelector(".frase_tercer");
-                    let autor = document.querySelector(".autor_tercer")
-                    let foto = document.querySelector(".img_tercer")
-                    quote.innerHTML = result.reseñas
-                    autor.innerHTML = result.autor
-                    foto.src = result.foto
-                },
-                error: function ajaxError(jqXHR) {
-                    console.error('Error: ', jqXHR.responseText);
-                }
-
-            });
-        }, intervalo)
-    }
+// Funcion que realiza una peticion ajax dada como parametro y actualiza los elementos recibidos, todo esto lo realiza de forma asincronica sin bloquear la pagina mientras se espera la respuesta de la misma
+function getReview(url, reviewSelector, authorSelector, photoSelector) {
+    $.ajax({
+        method: 'GET',
+        url: url,
+        contentType: 'application/json',
+        success: function (result) {
+            let quote = document.querySelector(reviewSelector);
+            let author = document.querySelector(authorSelector);
+            let photo = document.querySelector(photoSelector);
+            quote.innerHTML = result.reseñas;
+            author.innerHTML = result.autor;
+            photo.src = result.foto;
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
 }
+
+// Url para las peticiones ajax agregando la funcion numeroRandom para generar aleatoriamente las distintas reseñas
+let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+
+// Función encargada de la animacion del slider
+function moveSlider(sliderContainer, sliderItems, width, interval) {
+    let counter = 1;
+    setInterval(function tiempo() {
+        sliderContainer.style.transform = 'translate(' + (-width * counter) + 'px)';
+        sliderContainer.style.transition = 'transform 1s';
+        counter++;
+
+        if (counter == sliderItems.length) {
+            counter = 0;
+            setTimeout(function () {
+                sliderContainer.style.transform = 'translate(0px)';
+                sliderContainer.style.transition = 'transform 1s';
+                counter = 1;
+                updateReview();
+            }, interval)
+        }
+    }, interval);
+}
+
+// Se utiliza la funcion getReview para acutalizar los nodos 
+getReview(reviewUrl, '.review', '.author', '.photo');
+getReview(firstReviewUrl, '.first-review', '.first-author', '.first-photo');
+getReview(secondReviewUrl, '.second-review', '.second-author', '.second-photo');
+getReview(thirdReviewUrl, '.third-review', '.third-author', '.third-photo');
+
+//Funcion que se ejecuta una vez que este cargada la pagina y todos los elementos
+window.onload = function () {
+    let sliderContainer = document.querySelector('.slider-contenedor');
+    let sliderItems = document.querySelectorAll('.slider-test');
+    let width = sliderItems[0].clientWidth;
+    let interval = 100000;
+
+    moveSlider(sliderContainer, sliderItems, width, interval);
+};
+
+//Funcion encargada de actualizar los datos una vez que se llega al ultimo slide
+function updateReview() {
+    let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+    let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+    let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+    let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+
+    getReview(reviewUrl, '.review', '.author', '.photo');
+    getReview(firstReviewUrl, '.first-review', '.first-author', '.first-photo');
+    getReview(secondReviewUrl, '.second-review', '.second-author', '.second-photo');
+    getReview(thirdReviewUrl, '.third-review', '.third-author', '.third-photo');
+
+}
+
 
 
 
