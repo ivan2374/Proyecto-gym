4fee9d10d33cf2022c7169958a29cf139eee783a
ivan2374 <ivanmartines@hotmail.com>
2023-04-22 12:32:53 -0300

    Se arregle bug responsive del slider animado

--- reseñas.js
+++ reseñas.js
@@ -2,7 +2,7 @@
 let numeroRandom = (min, max) =>
     Math.floor(Math.random() * (max - min)) + min;
 
-// Funcion que realiza una peticion ajax dada como parametro y actualiza los elementos recibidos, todo esto lo realiza de forma asincronica sin bloquear la pagina mientras se espera la respuesta de la misma
+// Funcion que realiza una peticion ajax a una url dada como parametro y actualiza los elementos recibidos, todo esto lo realiza de forma asincronica sin bloquear la pagina mientras se espera la respuesta de la misma
 function getReview(url, reviewSelector, authorSelector, photoSelector) {
     $.ajax({
         method: 'GET',
@@ -23,30 +23,10 @@ function getReview(url, reviewSelector, authorSelector, photoSelector) {
 }
 
 // Url para las peticiones ajax agregando la funcion numeroRandom para generar aleatoriamente las distintas reseñas
-let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-
-// Función encargada de la animacion del slider
-function moveSlider(sliderContainer, sliderItems, width, interval) {
-    let counter = 1;
-    setInterval(function tiempo() {
-        sliderContainer.style.transform = 'translate(' + (-width * counter) + 'px)';
-        sliderContainer.style.transition = 'transform 1s';
-        counter++;
-
-        if (counter == sliderItems.length) {
-            counter = 0;
-            setTimeout(function () {
-                sliderContainer.style.transform = 'translate(0px)';
-                sliderContainer.style.transition = 'transform 1s';
-                counter = 1;
-                updateReview();
-            }, interval)
-        }
-    }, interval);
-}
+let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
 
 // Se utiliza la funcion getReview para acutalizar los nodos 
 getReview(reviewUrl, '.review', '.author', '.photo');
@@ -54,28 +34,47 @@ getReview(firstReviewUrl, '.first-review', '.first-author', '.first-photo');
 getReview(secondReviewUrl, '.second-review', '.second-author', '.second-photo');
 getReview(thirdReviewUrl, '.third-review', '.third-author', '.third-photo');
 
-//Funcion que se ejecuta una vez que este cargada la pagina y todos los elementos
-window.onload = function () {
-    let sliderContainer = document.querySelector('.slider-contenedor');
-    let sliderItems = document.querySelectorAll('.slider-test');
-    let width = sliderItems[0].clientWidth;
-    let interval = 5000;
-
-    moveSlider(sliderContainer, sliderItems, width, interval);
-};
-
 //Funcion encargada de actualizar los datos una vez que se llega al ultimo slide
 function updateReview() {
-    let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-    let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-    let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
-    let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 31)}.json`;
+    let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+    let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+    let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+    let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
 
     getReview(reviewUrl, '.review', '.author', '.photo');
     getReview(firstReviewUrl, '.first-review', '.first-author', '.first-photo');
     getReview(secondReviewUrl, '.second-review', '.second-author', '.second-photo');
     getReview(thirdReviewUrl, '.third-review', '.third-author', '.third-photo');
+}
 
+let slider = document.querySelector(".slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".slider-test");
+let contador = 1
+let tamanoWidth = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
+
+window.addEventListener("resize", function () {
+    tamanoWidth = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function tiempo() {
+    slices();
+}, intervalo);
+
+function slices() {
+    slider.style.transform = 'translate(' + (- tamanoWidth * contador) + 'px)';
+    slider.style.transition = 'transform 1s';
+    contador++;
+
+    if (contador == sliderIndividual.length) {
+        contador = 0;
+        setTimeout(function () {
+
+            slider.style.transform = 'translate(0px)';
+            slider.style.transition = 'transform 1s';
+            updateReview();
+        }, intervalo)
+    }
 }
 
 
